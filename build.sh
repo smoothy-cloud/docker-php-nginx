cb586994ae459655c36d192ab30ad9780a559f3e
Michiel Kempen <kempenmichiel@gmail.com>
2020-01-30 21:41:42 +0100

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ for dir in */ ; do
     image=`basename "$dir"`
     for subdir in $dir*/ ; do
         tag=$(echo `basename "$subdir"` | sed 's/\-/\./g')
-        docker build --no-cache --tag smoothy/$image:$tag ./$subdir
+        docker build --tag smoothy/$image:$tag ./$subdir
         docker push smoothy/$image:$tag
     done
 done
